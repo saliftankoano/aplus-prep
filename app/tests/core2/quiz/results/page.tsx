54a642bc12ff246bc729ece22f131ceb798998c4
salif <tanksalif@gmail.com>
2025-08-07 17:36:36 -0400

    Update results page with new logo and remove metadata from client component

--- app/tests/core2/quiz/results/page.tsx
+++ app/tests/core2/quiz/results/page.tsx
@@ -45,7 +45,9 @@ function QuizResultsContent() {
           <div className="flex items-center justify-between">
             <Link href="/tests" className="flex items-center">
               <div className="w-8 h-8 bg-gradient-to-br from-purple-500 to-blue-600 rounded-lg flex items-center justify-center">
-                <span className="text-white font-bold text-lg">A+</span>
+                <svg className="w-5 h-5 text-white" fill="none" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14.5 13.5V5.41a1 1 0 0 0-.3-.7L9.8.29A1 1 0 0 0 9.08 0H1.5v13.5A2.5 2.5 0 0 0 4 16h8a2.5 2.5 0 0 0 2.5-2.5m-1.5 0v-7H8v-5H3v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1M9.5 5V2.12L12.38 5zM5.13 5h-.62v1.25h2.12V5zm-.62 3h7.12v1.25H4.5zm.62 3h-.62v1.25h7.12V11z" clipRule="evenodd" fill="currentColor" fillRule="evenodd"/>
+                </svg>
               </div>
             </Link>
             
